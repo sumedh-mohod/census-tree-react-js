adb4b6d91c92262f1d3cb4b8947602e85f9fa375
Anjali <anjali.kothekar@orangebitsindia.com>
2022-06-09 18:38:23 +0530

    Issue Fixed #7 While creating new User it Accepts mob. no more than 10 digit

--- src/pages/NewUserForm.js
+++ src/pages/NewUserForm.js
@@ -563,7 +563,7 @@ const validateRole = () => {
       middleName: Yup.string().required('Middle Name is required'),
       lastName: Yup.string().required('Last Name is required'),
       email:Yup.string().email('Email must be a valid email address').required('Email is required'),
-      mobile: Yup.string().matches(phoneRegExp, 'Phone number is not valid').required('Email is required'),
+      mobile: Yup.string().matches(/^[0-9]\d{9}$/, 'Phone number is not valid').required('Email is required'),
       addressLine1: Yup.string().required('Address Line 1 is required'),
       city: Yup.string().required('City is required'),
       district: Yup.string().required('Districts is required'),
