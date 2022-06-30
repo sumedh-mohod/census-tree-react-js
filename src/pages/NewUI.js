77a8c5edb01d3a51cc6ad98f8f5a10e91ea560c2
unknown <latika.khatavkar@orangebitsindia.com>
2022-06-30 11:18:34 +0530

    Form added on update button in new UI

--- src/pages/NewUI.js
+++ src/pages/NewUI.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import {
     Stack,
     Button,
@@ -6,35 +8,30 @@ import {
     Grid,
     Box,
     Container,
-    Table,
-    TableRow,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
     Drawer,
     Divider,
+    TextField,
+    form,
   } from '@mui/material';
   import FilterAltRoundedIcon from '@mui/icons-material/FilterAltRounded';
   import  ImageGallery  from 'react-image-gallery';
   import Page from '../components/Page';
  import FilterDrawer from './FilterDrawer';
 
-  function createData(id, treeNumber, treeName) {
-    return { id, treeNumber, treeName };
-  }
+
+
   
   const rows = [
-    createData(1, 112300001, 'Neem'),
-    createData(2, 112300002, 'Neem'),
-    createData(3, 112300003, 'Neem'),
-    createData(4, 112300004, 'Neem'),
-    createData(5, 112300005, 'Neem'),
-    createData(6, 112300006, 'Neem'),
-    createData(7, 112300007, 'Neem'),
-    createData(8, 112300008, 'Neem'),
-    createData(9, 112300009, 'Neem'),
-    createData(10, 1123000010, 'Neem'),
+    (1, 112300001, 'Neem'),
+    (2, 112300002, 'Neem'),
+    (3, 112300003, 'Neem'),
+    (4, 112300004, 'Neem'),
+    (5, 112300005, 'Neem'),
+    (6, 112300006, 'Neem'),
+    (7, 112300007, 'Neem'),
+    (8, 112300008, 'Neem'),
+    (9, 112300009, 'Neem'),
+    (10, 1123000010, 'Neem'),
   ];
 
   
@@ -42,6 +39,7 @@ import {
   
   export default function NewUI(){
 
+    const [updateClick, setUpdateClick] = React.useState(false);
     const [state, setState] = React.useState({
       top: false,
       left: false,
@@ -88,10 +86,75 @@ import {
         ]
       };
 
+      const TreeDetailsSchema = Yup.object().shape(
+        {
+          locationType: Yup.string().required('Location Type is required'),
+          propertyType: Yup.string().required('Property Type is required'),
+          treeNumber: Yup.string().required('Tree Number is required'),
+          propertyOwner:Yup.string().required('Property Owner is required'),
+          tenantName: Yup.string().required('Tenant Name is required'),
+          area: Yup.string().required('Area is required'),
+          treeType: Yup.string().required('Tree Type is required'),
+          localtreeName: Yup.string().required('Tree Name(Local) is required'),
+          botTreeName: Yup.string().required('Tree Name(Botanical) is required'),
+          girth: Yup.string().required('Girth is required'),
+          height: Yup.string().required('Height is required'),
+          canopy: Yup.string().required('Canopy is required'),
+          treeCondition: Yup.string().required('Tree Condition is required'),
+          disease: Yup.string().required('Disease is required'),
+          plantationDate:Yup.string().required('Plantation Date is required'),
+          referredExpert: Yup.string().matches(/^[0-9]\d{9}$/, 'Phone number is not valid').required('Referred To Expert is required'),
+          actionTaken: Yup.string().required('Action To Be Taken is required'),
+          images: Yup.string().required('Upload images'),
+          qcStatus: Yup.string().required('QC Status is required'),
+          qcBy: Yup.string().required('QC By is required'),
+          qcOnDate: Yup.string().required('QC On Date is required'),
+      }
+      );
+
+      const {
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        values,
+        errors,
+        touched,
+        setFieldValue,
+      } = useFormik({
+        enableReinitialize: true,
+        initialValues: {
+          locationType:	"Desert",
+          propertyType:	"Mall",
+          treeNumber:	"11100011",
+          propertyOwner:	"cricket",
+          tenantName:	"hockey",
+          area:	"200",
+          treeType:	"Coniferous trees",
+          localtreeName:	"Christmas trees",
+          botTreeName:	"Araucaria columnaris",
+          girth:	"100",
+          height:	"20",
+          canopy:	"10",
+          treeCondition:	"Fully cut",
+          disease:	"Leaf Rusts",
+          plantationDate:	"2022/06/12",
+          referredExpert:	"Yes",
+          actionTaken:	"no",
+          images: "",
+          qcStatus:	"Pending",
+          qcBy:	"-",
+          qcOnDate: "-"
+        },
+        validationSchema: TreeDetailsSchema,
+        onSubmit: (values) => {
+          console.log(values);
+        },
+      });
+
     return(
         <Page title="New UI">
             <Container>
-          
+            <Box sx={{  height: '100' }}>
           <Button
             
            variant='outlined'
@@ -118,6 +181,7 @@ import {
         >
          <FilterDrawer data={toggleDrawer("right", false)}/>
         </Drawer>
+        </Box>
         <Grid container spacing={2} sx={{ mt:5 }}>
   
   <Grid item xs={6}>
@@ -198,18 +262,349 @@ import {
     <Typography variant="h5" gutterBottom>
             Tree Details: 
           </Typography>
-    <Typography>Tree Number: 11200004</Typography>
-    <Typography>Tree Name: Aam</Typography>
-    <Typography>Tree Condition: Good</Typography>
-    <Typography>Tree Diseases: No</Typography>
-    </Box>
-    <Box sx={{ height: 200, width: '100%', mt:5 }}>
+          {!updateClick?(
+            <>
+             <Typography>Tree Number: 11200004</Typography>
+             <Typography>Tree Name: Aam</Typography>
+             <Typography>Tree Condition: Good</Typography>
+             <Typography>Tree Diseases: No</Typography>
+             <Box sx={{ height: 200, width: '100%', mt:5 }}>
     <Stack direction="row" spacing={4}>
   <Button variant="outlined" size="small">Approve & Next</Button>
-  <Button variant="outlined" size="small">Unapprove & Update</Button>
+  <Button variant="outlined" size="small" onClick={()=>setUpdateClick(true)}>Unapprove & Update</Button>
   <Button variant="outlined" size="small">Refer To Expert</Button>
 </Stack>
 </Box>
+             </>
+          ):(
+          <>
+          <form onSubmit={handleSubmit}>
+    <TextField
+                fullWidth
+                margin="normal"
+                name="locationType"
+                placeholder="Location Type"
+                label="Location Type"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.locationType || ""}
+                required
+                helperText={
+     
+                    errors.locationType && touched.locationType
+                      
+                
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="propertyType"
+                placeholder="Property Type"
+                label="Property Type"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.propertyType || ""}
+                required
+                helperText={
+                
+                    errors.propertyType && touched.propertyType
+                     
+              
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="treeNumber"
+                placeholder="Tree Number"
+                label="Tree Number"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.treeNumber || ""}
+                required
+                helperText={
+                    errors.treeNumber && touched.treeNumber
+                     
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="propertyOwner"
+                placeholder="Property Owner"
+                label="Property Owner"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.propertyOwner || ""}
+                required
+                helperText={
+                    errors.propertyOwner && touched.propertyOwner
+                     
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="tenantName"
+                placeholder="Tenant Name"
+                label="Tenant Name"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.tenantName || ""}
+                required
+                helperText={
+                    errors.tenantName && touched.tenantName
+                     
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="area"
+                placeholder="Area"
+                label="Area"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.area || ""}
+                required
+                helperText={
+                    errors.area && touched.area
+                     
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="treeType"
+                placeholder="Tree Type"
+                label="Tree Type"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.treeType || ""}
+                required
+                helperText={
+                    errors.treeType && touched.treeType
+                     
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="localTreeName"
+                placeholder="Tree Name(local)"
+                label="Tree Name(local)"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.localTreeName || ""}
+                required
+                helperText={
+                    errors.localTreeName && touched.localTreeName
+                      
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="BotTreeName"
+                placeholder="Tree Name(Botanical)"
+                label="Tree Name(Botanical)"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.BotTreeName || ""}
+                required
+                helperText={
+                    errors.BotTreeName && touched.BotTreeName
+                    
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="girth"
+                placeholder="Girth"
+                label="Girth"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.girth || ""}
+                required
+                helperText={
+                    errors.girth && touched.girth
+                     
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="height"
+                placeholder="Height"
+                label="Height"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.height || ""}
+                required
+                helperText={
+                    errors.height && touched.height
+                      
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="canopy"
+                placeholder="Canopy"
+                label="Canopy"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.canopy || ""}
+                required
+                helperText={
+                    errors.canopy && touched.canopy
+                     
+                }
+              />
+                            <TextField
+                fullWidth
+                margin="normal"
+                name="treeCondition"
+                placeholder="Tree Condition"
+                label="Tree Condition"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.treeCondition || ""}
+                required
+                helperText={
+                    errors.treeCondition && touched.treeCondition
+                     
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="disease"
+                placeholder="Disease"
+                label="Disease"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.disease || ""}
+                required
+                helperText={
+                    errors.disease && touched.disease
+                    
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="plantationDate"
+                placeholder="Plantation Date"
+                label="Plantation Date"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.plantationDate || ""}
+                required
+                helperText={
+                    errors.plantationDate && touched.plantationDate
+                     
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="referredExpert"
+                placeholder="Referred To Expert"
+                label="Referred To Expert"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.referredExpert || ""}
+                required
+                helperText={
+                    errors.referredExpert && touched.referredExpert
+                    
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="actionTaken"
+                placeholder="Action Need To Be Taken"
+                label="Action Need To Be Taken"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.actionTaken || ""}
+                required
+                helperText={
+                    errors.actionTaken && touched.actionTaken
+                  
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="qcStatus"
+                placeholder="QC Status"
+                label="QC Status"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.qcStatus || ""}
+                required
+                helperText={
+                    errors.qcStatus && touched.qcStatus
+                      
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="qcBy"
+                placeholder="QC By"
+                label="QC By"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.qcBy || ""}
+                required
+                helperText={
+                    errors.qcBy && touched.qcBy
+                    
+                }
+              />
+              <TextField
+                fullWidth
+                margin="normal"
+                name="qcOnDate"
+                placeholder="QC On Date"
+                label="QC On Date"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.qcOnDate || ""}
+                required
+                helperText={
+                    errors.qcOnDate && touched.qcOnDate
+                    
+                }
+              />
+          <Button
+                
+                  variant="contained"
+                  type="submit"
+                  fullWidth
+                
+                >
+                  Update
+                </Button>
+              
+              </form>
+    </>
+    )}
+    </Box>
+    
+    {/* <Box sx={{ height: 200, width: '100%', mt:5 }}>
+    <Stack direction="row" spacing={4}>
+  <Button variant="outlined" size="small">Approve & Next</Button>
+  <Button variant="outlined" size="small" onClick={()=>setUpdateClick(true)}>Unapprove & Update</Button>
+  <Button variant="outlined" size="small">Refer To Expert</Button>
+</Stack>
+</Box> */}
   </Box>
 </Stack>
   </Grid>
