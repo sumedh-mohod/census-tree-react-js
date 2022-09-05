8111b9faba1dcd2d10d03699c5be0aff7e2394ad
chetna.musale@orangebitsindia.com <chetna.musale@orangebitsindia.com>
2022-09-05 14:10:01 +0530

    'date_remove'

--- src/pages/Reports/TreeReports.js
+++ src/pages/Reports/TreeReports.js
@@ -1,64 +1,22 @@
-import React from 'react';
+import React from 'react'
+import Typography from '@mui/material/Typography';
 import { green } from '@mui/material/colors';
 import Icon from '@mui/material/Icon';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
-import {
-  Card,
-  Table,
-  Stack,
-  Avatar,
-  Button,
-  Checkbox,
-  TableRow,
-  TableBody,
-  TableCell,
-  Container,
-  Typography,
-  TableContainer,
-  Pagination,
-  Link,
-  IconButton,
-  Grid
-} from '@mui/material';
-import Page from '../../components/Page';
-import { Piechart } from './Chart/Piechart';
-import Barchart from './Chart/Barchart';
 
 function TreeReports() {
   return (
-    <>
-      <Page title="User">
-        <Container>
-          <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-            <div role="presentation">
-              <Breadcrumbs aria-label="breadcrumb" style={{ color: '#000000' }} separator=">">
-                <Typography variant="h4" gutterBottom style={{ color: '#000000' }}>
-                  Tree Data
-                </Typography>
-                <Typography variant="h4" gutterBottom style={{ color: '#000000' }}>
-                  Reports
-                </Typography>
-              </Breadcrumbs>
-            </div>
-          </Stack>
-          {/* <Card>
-            <Barchart />
-            <br />
-            <Grid container spacing={0} >
-            <Grid item xs={3} >
-              </Grid>
-              <Grid item xs={6} >
-              <Piechart />
-              </Grid>
-              <Grid item xs={3} >
-              </Grid>
-            </Grid>
-          
-          </Card> */}
-        </Container>
-      </Page>
-    </>
-  );
+    <div role="presentation" >
+    <Breadcrumbs aria-label="breadcrumb" style={{color: "#000000"}} separator='>'>
+        <Typography variant="h4" gutterBottom style={{color: "#000000"}}>
+          Reports
+        </Typography>
+        <Typography variant="h4" gutterBottom style={{color: "#000000"}}>
+     Tree Reports
+        </Typography>
+    </Breadcrumbs>
+  </div>
+  )
 }
 
-export default TreeReports;
+export default TreeReports
